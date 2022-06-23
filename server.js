b914520f133bb662f4bcb10da97ba7f9bb4424c9
thaoly258 <joly8147@gmail.com>
2022-06-22 18:39:35 -0700

    added content to index.ejs

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express()
 const cors = require('cors')
 const MongoClient = require('mongodb').MongoClient
 require('dotenv').config()
+const PORT = 8000
 
 
 let db,
@@ -16,6 +17,12 @@ MongoClient.connect(dbConnectionString)
         db = client.db(dbName)
         collection = db.collection('shipwrecks')
     })
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+app.use(cors())
 
 app.listen(process.env.PORT || PORT, () => {
     console.log(`server is running on port`)
